Fixed camera model code in scripts - Changes in naming of some camera parameters - Fixed rotation and coordinate transform matrices - Added option for choosing intrinsic or extrinsic rotation for objects and camera in scene - Fixed projection model - Minor code improvements

scene_objects.py
@@ -1,4 +1,5 @@
 import numpy as np
+
 from utils import get_rotation_matrix
 
 class Point3d:
@@ -18,8 +19,9 @@ class Plane:
 
 class Box:
 
-    def __init__(self, x, y, z, w, h, d, roll, pitch, yaw) -> None:
-        """Constructor of a class describing a box
+    def __init__(self, x, y, z, w, h, d, roll, pitch, yaw, extrinsic_angles=False) -> None:
+        """
+        Constructor of a class describing a box
 
         Args:
             x (double): x-coordinate of the center of a box, m
@@ -62,8 +64,8 @@ class Box:
         ]
         points = np.array(points).T
         # rotate points
-        self.R = get_rotation_matrix(roll, pitch, yaw)
-        self.points = np.dot(self.R, points) + np.array([[x, y, z]]).T
+        self.rotation_matrix = get_rotation_matrix(roll, pitch, yaw, extrinsic=extrinsic_angles)
+        self.points = np.dot(self.rotation_matrix, points) + np.array([[x, y, z]]).T
         # plane is defined by a point on it and a normal vector
         # so each plane is defined by normal vector and 4 points
         # estimate normal vector for each plane as a vector product of two edges constructing a plane
@@ -107,11 +109,12 @@ class Box:
         # Check if intersection point is within rectangle
         if 0 <= x[0] <= 1:
             if 0 <= x[1] <= 1:
-                return l0 + ray*x[2]
-        return l0 + ray*x[2,1] if debug else None
+                return l0 + ray * x[2]
+        return l0 + ray * x[2] if debug else None
 
     def find_all_intersections(self, ray, p0):
-        """Find all intersection points of box with given ray
+        """
+        Find all intersection points of box with given ray
 
         Args:
             ray (ndarray): (x, y, z)-components of a ray

utils.py
@@ -48,10 +48,10 @@ def draw_xz_projection_for_a_plane(plane, image, origin, color, ray=None):
         cv2.imshow("", image)
         cv2.waitKey(0)
 
-def get_rotation_matrix(roll, pitch, yaw):
-    yaw_rad = np.deg2rad(yaw)
-    pitch_rad = np.deg2rad(pitch)
-    roll_rad = np.deg2rad(roll)
+def get_rotation_matrix(roll, pitch, yaw, extrinsic=False):
+    yaw_rad = yaw
+    pitch_rad = pitch
+    roll_rad = roll
     # roll
     R_roll = np.array([
         [np.cos(roll_rad), -np.sin(roll_rad), 0.0],
@@ -60,9 +60,9 @@ def get_rotation_matrix(roll, pitch, yaw):
     ])
     # yaw
     R_yaw = np.array([
-        [np.cos(yaw_rad), 0.0, -np.sin(yaw_rad)],
+        [np.cos(yaw_rad), 0.0, np.sin(yaw_rad)],
         [0.0, 1.0, 0.0],
-        [np.sin(yaw_rad), 0.0, np.cos(yaw_rad)]  
+        [-np.sin(yaw_rad), 0.0, np.cos(yaw_rad)]  
     ])
     # pitch
     R_pitch = np.array([
@@ -70,6 +70,8 @@ def get_rotation_matrix(roll, pitch, yaw):
         [0.0, np.cos(pitch_rad), -np.sin(pitch_rad)],
         [0.0, np.sin(pitch_rad), np.cos(pitch_rad)]  
     ])
-
-    R = np.dot(R_yaw, np.dot(R_pitch, R_roll))
+    if extrinsic:
+        R = np.dot(R_yaw, np.dot(R_pitch, R_roll))
+    else:
+        R = np.dot(R_roll, np.dot(R_pitch, R_yaw))
     return R